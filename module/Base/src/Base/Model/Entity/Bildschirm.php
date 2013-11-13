81c9ac4b60b321125191f3883f0e360787264fdb
sumeragi <sumeragi@fantasymail.de>
2013-11-13 14:22:43 +0100

    add infoscripts with different displays

---
+++ module/Base/src/Base/Model/Entity/Bildschirm.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Base\Model\Entity;
+
+class Bildschirm {
+    
+    /**
+     * $id eindeutige Id des Bildschirms
+     * @var int
+     */
+    private $id;
+    
+    /**
+     * $beschreibung Beschreibung des Bildschirms
+     * @var string
+     */
+    private $beschreibung;
+    
+    /**
+     * $inserate enthält Referenzen zu Inseraten, die auf diesem Bildschirm
+     * angezeigt werden.
+     * @var array
+     */
+//    private $inserate = array();
+    
+    
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+        return $this;
+    }
+
+    public function getBeschreibung() {
+        return $this->beschreibung;
+    }
+
+    public function setBeschreibung($beschreibung) {
+        $this->beschreibung = $beschreibung;
+        return $this;
+    }
+
+//    public function getInserate() {
+//        return $this->inserate;
+//    }
+//
+//    public function setInserate(array $inserate) {
+//        $this->inserate = $inserate;
+//        return $this;
+//    }
+
+
+    
+}
---
+++ module/Base/src/Base/Model/Entity/Inserat.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Base\Model\Entity;
+
+class Inserat {
+
+    /**
+     * inseratId eindeutige Id des Inserats
+     * @var int
+     */
+    private $inseratId;
+
+    /**
+     * $start Startdatum des Inserats. Gibt an, ab wann ein Inserat angezeigt 
+     * wird.
+     * @var string
+     */
+    private $start;
+
+    /**
+     * $ende Enddatum des Inserats. Gibt das Datum des letzten Tags an, an dem 
+     * ein Inserat noch angezeigt wird.
+     * @var string
+     */
+    private $ende;
+
+    /**
+     * $url Url- Adresse des Inhalts eines Inserats
+     * @var string
+     */
+    private $url;
+
+    /**
+     * $aktiv Gibt an, ob ein Inserat von einem berechtigten Nutzer 
+     * freigeschaltet wurde.
+     * @var boolean
+     */
+    private $aktiv;
+
+    /**
+     * $bildschirme enthält Bildschirm- Ids, der Bildschirme auf denen
+     * ein Inserat angezeigt wird.
+     * @var array 
+     */
+    private $bildschirme = array();
+    
+    /**
+     * $userId ist die Id des Benutzers, zu dem ein Inserat gehört
+     * @var int
+     */
+    private $userId;
+
+    public function getInseratId() {
+        
+        return $this->inseratId;
+    }
+
+    public function setInseratId($inseratId) {
+        
+        $this->inseratId = $inseratId;
+        return $this;
+    }
+
+    
+    public function getStart() {
+        
+        return $this->start;
+    }
+
+    public function setStart($start) {
+        
+        $this->start = $start;
+        return $this;
+    }
+
+    
+    public function getEnde() {
+        
+        return $this->ende;
+    }
+
+    public function setEnde($ende) {
+
+        $this->ende = $ende;
+        return $this;
+    }
+
+    
+    public function getUrl() {
+
+        return $this->url;
+    }
+
+    public function setUrl($url) {
+
+        $this->url = $url;
+        return $this;
+    }
+
+    
+    public function getAktiv() {
+
+        return $this->aktiv;
+    }
+
+    public function setAktiv($aktiv) {
+
+        $this->aktiv = $aktiv;
+        return $this;
+    }
+
+    
+    public function getBildschirme() {
+
+        return $this->bildschirme;
+    }
+
+    public function setBildschirme(array $bildschirme) {
+
+        $this->bildschirme = $bildschirme;
+        return $this;
+    }
+
+    public function addBildschirm($bildschirm) {
+
+        array_push($this->bildschirme, $bildschirm);
+        return $this;
+    }
+    
+    
+    public function getUserId() {
+        return $this->userId;
+    }
+
+    public function setUserId($userId) {
+        $this->userId = $userId;
+        return $this;
+    }
+
+
+}
