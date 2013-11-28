da8fd956b0280d73532cb8400ce1ae339cd1332a
sumeragi <sumeragi@fantasymail.de>
2013-11-28 15:26:48 +0100

    erweitern des Infoscript Entity mit Text und TextLine

--- module/Base/src/Base/Model/Entity/Infoscript.php
+++ module/Base/src/Base/Model/Entity/Infoscript.php
@@ -4,17 +4,137 @@ namespace Base\Model\Entity;
 
 class Infoscript extends Inserat {
 
-    protected $titel;
-    
-    
-    public function getTitel() {
-        return $this->titel;
+    /**
+     *
+     * @var string
+     */
+    protected $headLine;
+
+    /**
+     *
+     * @var string
+     */
+    protected $title;
+
+    /**
+     *
+     * @var array
+     */
+    protected $text = array();
+
+    /**
+     *
+     * @var array
+     */
+    protected $lists = array();
+
+
+    /**
+     *
+     * @return string
+     */
+    public function getHeadLine() {
+        return $this->headLine;
     }
 
-    public function setTitel($titel) {
-        $this->titel = $titel;
+    /**
+     *
+     * @param string $headLine
+     * @return \Base\Model\Entity\Infoscript
+     */
+    public function setHeadLine($headLine) {
+        $this->headLine = $headLine;
         return $this;
     }
 
+
+
+    /**
+     *
+     * @return string
+     */
+    public function getTitle() {
+        return $this->title;
+    }
+
+    /**
+     *
+     * @param string $title
+     * @return \Base\Model\Entity\Infoscript
+     */
+    public function setTitle($title) {
+        $this->title = $title;
+        return $this;
+    }
+
+
+
+    /**
+     *
+     * @return array
+     */
+    public function getText() {
+        return $this->text;
+    }
+
+    /**
+     *
+     * @param string $text
+     * @return \Base\Model\Entity\Infoscript
+     */
+    public function setText($text) {
+
+        $textLineArray = explode(PHP_EOL, $text);
+
+        foreach ($textLineArray as $textLine) {
+            $this->addTextLine($textLine);
+        }
+
+        return $this;
+    }
+
+    /**
+     *
+     * @param string $textLine
+     * @return \Base\Model\Entity\Infoscript
+     */
+    public function addTextLine($textLine) {
+
+        $txtLine = new TextLine($textLine);
+
+        array_push($this->text, $txtLine);
+        return $this;
+    }
+
+
+
+    /**
+     *
+     * @return array
+     */
+    public function getLists() {
+        return $this->lists;
+    }
+
+    /**
+     *
+     * @param array $lists
+     * @return \Base\Model\Entity\Infoscript
+     */
+    public function setLists(array $lists) {
+        $this->lists = $lists;
+        return $this;
+    }
+
+    /**
+     *
+     * @param string $list
+     * @return \Base\Model\Entity\Infoscript
+     */
+    public function addList($list) {
+
+        array_push($this->lists, $list);
+        return $this;
+    }
 
 }
